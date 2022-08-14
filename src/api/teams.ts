363f9e69d1e1d9770d6389e8fb93eb42df65cac1
zaydam1000000 <86502397+zaypers@users.noreply.github.com>
2022-08-14 15:26:45 -0700

    added option for a program to teams.get()

--- src/api/teams.ts
+++ src/api/teams.ts
@@ -1,5 +1,12 @@
 import request from "../request"
 
-export async function get(number: String): Promise<JSON> {
-  return await request(`teams?number%5B%5D=${number}`)
+export async function get(number: String, program: String = ""): Promise<JSON> {
+  let reqStr = `teams?number%5B%5D=${number}`
+
+  if (program == 'VRC') reqStr += `&program%5B%5D=1`
+  else if (program == 'VEXU') reqStr += `&program%5B%5D=4`
+  else if (program == 'VIQC') reqStr += `&program%5B%5D=41`
+  else if (program == 'WORKSHOP') reqStr += `&program%5B%5D=37`
+
+  return await request(reqStr)
 }
