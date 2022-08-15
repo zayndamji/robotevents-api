f1be124e7405989f17ccf0c115d4c1854e4a3a5b
zaydam1000000 <86502397+zaypers@users.noreply.github.com>
2022-08-15 09:48:01 -0700

    moved programs to seperate file in data/ dir

--- src/api/teams.ts
+++ src/api/teams.ts
@@ -1,19 +1,5 @@
 import request from "../request"
-
-export const programs = {
-  "VRC": 1,
-  "VEXU": 4,
-  "WORKSHOP": 37,
-  "VIQC": 41,
-  "NRL": 43,
-  "ADC": 44,
-  "TVRC": 46,
-  "TIQC": 47,
-  "VRAD": 51,
-  "BellAVR": 55,
-  "FAC": 56,
-  "VAIC": 57
-}
+import { programs } from "../data/programs"
 
 /**
  * Fetches team by number and program.
---
+++ src/data/programs.ts
@@ -0,0 +1,14 @@
+export const programs = {
+  "VRC": 1,
+  "VEXU": 4,
+  "WORKSHOP": 37,
+  "VIQC": 41,
+  "NRL": 43,
+  "ADC": 44,
+  "TVRC": 46,
+  "TIQC": 47,
+  "VRAD": 51,
+  "BellAVR": 55,
+  "FAC": 56,
+  "VAIC": 57
+}
