e81f1bf09f4c06ab9dfb77cd4902e2c984cf1d2e
zaydam1000000 <86502397+zaypers@users.noreply.github.com>
2022-09-06 21:08:26 -0700

    removed data and funcs from exposed api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * as teams from './api/teams'
 export * as events from './api/events'
-export * as data from './data'
-export * as funcs from './funcs'
 export const version: string = '0.2.4'
