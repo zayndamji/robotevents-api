597ff43a4ac4eb9e19191cb2b2f9174bb79d1ece
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2024-08-20 08:49:53 -0700

    change season/program names to rebranded names

--- src/api/programs.ts
+++ src/api/programs.ts
@@ -1,15 +1,15 @@
 export enum Programs {
   DEFAULT = 0,
-  VRC = 1,
-  VEXU = 4,
+  V5RC = 1,
+  VURC = 4,
   WORKSHOP = 37,
-  VIQC = 41,
+  VIQRC = 41,
   NRL = 43,
   ADC = 44,
   TVRC = 46,
-  TIQC = 47,
+  TVIQRC = 47,
   VRAD = 51,
   BELLAVR = 55,
   FAC = 56,
-  VAIC = 57
+  VAIRC = 57
 }
